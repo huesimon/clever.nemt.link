c71ecaa43ba6be9cbdfe262fa2c98d2f436da51c
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2022-11-03 19:50:35 +0100

    jetstream install

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,8 @@ return new class extends Migration
             $table->string('telegram_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
