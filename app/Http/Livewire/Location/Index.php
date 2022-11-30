2ad61bddd64f2d81cc41456b3fb43b8f90c0d7ee
Simon <simon.d.rasmussen@gmail.com>
2022-11-30 14:19:50 +0100

    Fix search for favorites

--- app/Http/Livewire/Location/Index.php
+++ app/Http/Livewire/Location/Index.php
@@ -19,16 +19,14 @@ class Index extends Component
 
         if (str($this->search)->length() > 2) {
             $query->where('name', 'like', '%' . $this->search . '%');
-        }
-
-        if ($this->user) {
-            $query->favorited($this->user);
-        } else {
+        } elseif (!$this->user) {
             $locationIds = Charger::distinct()->orderBy('updated_at', 'desc')->limit(15)->get(['location_id', 'updated_at']);
             $query->whereIn('id', $locationIds->pluck('location_id'));
         }
 
-
+        if ($this->user) {
+            $query->favorited($this->user);
+        }
 
         return view('livewire.location.index', [
             'locations' => $query->get(),
