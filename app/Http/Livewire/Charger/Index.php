932b3519071625d977e1a8250764e9be5b7a2a0b
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2022-12-01 22:44:43 +0100

    trying to add reports page

---
+++ app/Http/Livewire/Charger/Index.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Livewire\Charger;
+
+use App\Models\Charger;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithPagination;
+
+    public $sortDesc = true;
+    public $filterByStatus = 'all';
+
+    public function render()
+    {
+        return view('livewire.charger.index',
+            [
+                'chargers' => Charger::whereHas('location', function ($query) {
+                    return $query->where('is_public_visible', 'Always');
+                })
+                ->with(['location' => function ($query) {
+                    return $query->where('is_public_visible', 'Always');
+                }])
+                ->when($this->filterByStatus != 'all', function ($query) {
+                    return $query->where('status', $this->filterByStatus);
+                })
+                ->orderBy('updated_at', request()->get('sort', $this->sortDesc ? 'desc' : 'asc'))
+                ->paginate(10),
+                'statuses' => Charger::STATUSES,
+                'chargersOutOfOrderCount' => Charger::where('status', Charger::OUT_OF_ORDER)->count(),
+                'chargersAvailableCount' => Charger::where('status', Charger::AVAILABLE)->count(),
+                'chargersOccupiedCount' => Charger::where('status', Charger::OCCUPIED)->count(),
+                'totalChargersCount' => Charger::count(),
+                'totalChargersCountLastWeek' => Charger::where('created_at', '>=', now()->subWeek())->count(),
+                'longChargingSessionsCount' => Charger::where('status', Charger::OCCUPIED)->where('updated_at', '<=', now()->subHours(8))->where('updated_at', '>=', now()->subHours(12))->count(),
+                'longerChargingSessionsCount' => Charger::where('status', Charger::OCCUPIED)->where('updated_at', '<=', now()->subHours(12))->count(),
+            ]
+        );
+    }
+
+    public function toggleDesc()
+    {
+        $this->sortDesc = !$this->sortDesc;
+    }
+}
---
+++ resources/views/livewire/charger/index.blade.php
@@ -0,0 +1,164 @@
+
+    <div>
+        <div class="mb-4">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">Useless Stats</h3>
+            <dl
+                class="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-y-0 md:divide-x">
+                <div class="px-4 py-5 sm:p-6">
+                    <dt class="text-base font-normal text-gray-900">Total Chargers</dt>
+                    <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
+                        <div class="flex items-baseline text-2xl font-semibold text-indigo-600">
+                            {{ $totalChargersCount }}
+                            <span class="ml-2 text-sm font-medium text-gray-500"> from {{ $totalChargersCountLastWeek }} </span>
+                        </div>
+                        @if ($totalChargersCount > $totalChargersCountLastWeek)
+                        <div
+                            class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
+                            <!-- Heroicon name: mini/arrow-up -->
+                            <svg class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <span class="sr-only"> Increased by %</span>
+                            {{ round((($totalChargersCount - $totalChargersCountLastWeek) / $totalChargersCountLastWeek) * 100, 2) }}%
+                        </div>
+                        @elseif ($totalChargersCount < $totalChargersCountLastWeek)
+                        <div
+                            class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-red-100 text-red-800 md:mt-2 lg:mt-0">
+                            <!-- Heroicon name: mini/arrow-down -->
+                            <svg class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M10 3a.75.75 0 01.75.75V14.388l3.46-4.157a.75.75 0 011.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 011.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <span class="sr-only"> Decreased by %</span>
+                            {{ round((($totalChargersCountLastWeek - $totalChargersCount) / $totalChargersCountLastWeek) * 100, 2) }}%
+                        @endif
+                    </dd>
+                </div>
+                <div class="px-4 py-5 sm:p-6">
+                    <dt class="text-base font-normal text-gray-900">Currently In Use</dt>
+                    <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
+                        <div class="flex items-baseline text-2xl font-semibold text-indigo-600">
+                            {{ $chargersOccupiedCount }}
+                            <span class="ml-2 text-sm font-medium text-gray-500">Remaning: {{$chargersAvailableCount}}</span>
+                        </div>
+
+                    </dd>
+                </div>
+                <div class="px-4 py-5 sm:p-6">
+                    <dt class="text-base font-normal text-gray-900">Long Charging Sessions</dt>
+                    <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
+                        <div class="flex items-baseline text-2xl font-semibold text-indigo-600">
+                            {{$longChargingSessionsCount}}
+                            <span class="ml-2 text-sm font-medium text-gray-500">8 hour plus: {{$longerChargingSessionsCount}}</span>
+                        </div>
+                        {{-- <div
+                            class="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-red-100 text-red-800 md:mt-2 lg:mt-0">
+                            <!-- Heroicon name: mini/arrow-down -->
+                            <svg class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <span class="sr-only"> Decreased by </span>
+                            4.05%
+                        </div> --}}
+                    </dd>
+                </div>
+            </dl>
+        </div>
+        {{--  --}}
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-xl font-semibold text-gray-900">Chargers</h1>
+                    <p class="mt-2 text-sm text-gray-700">A list of all the chargers with location name, status, timestamp.</p>
+                </div>
+                <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <button type="button"
+                        class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add
+                        user</button>
+                </div>
+            </div>
+            <div class="mt-8 flex flex-col">
+                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        {{-- options --}}
+                        <div class="">
+                            <select wire:model='filterByStatus' name="location" id="location" class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                @foreach ($statuses as  $status)
+                                    <option value="{{ $status }}">{{ $status }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name
+                                        </th>
+                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status
+                                        </th>
+                                        <th wire:click='toggleDesc' scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Timestamp {{$sortDesc ? "⬆️" : "⬇️"}} </th>
+                                        </th>
+                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role
+                                        </th>
+                                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+                                    @foreach ($chargers as $charger)
+                                    <tr>
+                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 ">
+                                            {{ $charger->location->name}}
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            @if ($charger->status == 'Available')
+                                            <span
+                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                Available
+                                            </span>
+                                            @elseif ($charger->status == 'Occupied')
+                                            <span
+                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                                Occupied
+                                            </span>
+                                            @else
+                                            <span
+                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                                {{ $charger->status ?? 'Unknown Status' }}
+                                            </span>
+                                            @endif
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            {{ $charger->updated_at->diffForHumans() }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
+                                        <td
+                                            class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
+                                                    class="sr-only">, Lindsay Walton</span></a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- More people... -->
+                                </tbody>
+                            </table>
+                        </div>
+                        {{ $chargers->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
