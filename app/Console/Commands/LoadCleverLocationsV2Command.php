40e22cd1aa5ed4fb245691e3cfe364f9f9d343ad
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2023-03-19 17:09:06 +0100

    🛠️ Load locations takes 17 seconds

--- app/Console/Commands/LoadCleverLocationsV2Command.php
+++ app/Console/Commands/LoadCleverLocationsV2Command.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Charger;
 use App\Models\Company;
 use App\Models\Location;
 use Illuminate\Console\Command;
@@ -53,12 +54,12 @@ class LoadCleverLocationsV2Command extends Command
         $cleverCollection->map(function ($chunk) {
                 return [
                     'external_id' => $chunk->locationId,
-                        'name' => $chunk->name,
-                        'company_id' => '1',
-                        'origin' => 'clever',
-                        'is_roaming_allowed' => $chunk->publicAccess->isRoamingAllowed,
-                        'is_public_visible' => $chunk->publicAccess->visibility === 'Always' ? true : false,
-                        'coordinates' => $chunk->coordinates->lat . ',' . $chunk->coordinates->lng,
+                    'name' => $chunk->name,
+                    'company_id' => '1',
+                    'origin' => 'clever',
+                    'is_roaming_allowed' => $chunk->publicAccess->isRoamingAllowed,
+                    'is_public_visible' => $chunk->publicAccess->visibility === 'Always' ? true : false,
+                    'coordinates' => $chunk->coordinates->lat . ',' . $chunk->coordinates->lng,
                 ];
             })
             ->chunk(1000)
@@ -67,9 +68,45 @@ class LoadCleverLocationsV2Command extends Command
                     $chunk->toArray(),
                     ['external_id'],
                     ['name', 'company_id']);
-                $bar->advance();
+                // $bar->advance();
             });
-        $bar->finish();
+        // $bar->finish();
+
+        $cleverCollection->map(function ($chunk) {
+            // $connectorMapResult[] = [
+            //     'location_external_id' => $chunk->locationId,
+            // ];
+            collect($chunk->evses)->map(function ($evses) use (&$connectorMapResult, $chunk) {
+                    collect($evses->connectors)->map(function ($connector) use (&$connectorMapResult, $chunk, $evses) {
+                        // dd($evses);
+                        $connectorMapResult[] = [
+                            'location_external_id' => $chunk->locationId,
+                            'balance' => $connector->balance,
+                            'evse_id' => $evses->evseId,
+                            'evse_connector_id' => $connector->evseConnectorId,
+                            'connector_id' => $connector->connectorId,
+                            'max_current_amp' => $connector->maxCurrentAmp ?? 0,
+                            'max_power_kw' => $connector->maxPowerKw,
+                            'plug_type' => $connector->plugType,
+                            'speed' => $connector->speed,
+                        ];
+                    });
+                });
+
+                return $connectorMapResult;
+            })
+            ->chunk(100)
+            ->each(function ($chunk) use ($cleverOperator, $bar) {
+                collect($chunk)->each(function ($chunk) use ($cleverOperator, $bar) {
+                    // dd($chunk);
+                    Charger::upsert(
+                        $chunk,
+                        ['evse_id'],
+                        ['location_external_id', 'balance', 'evse_connector_id', 'connector_id', 'max_current_amp', 'max_power_kw', 'plug_type', 'speed']);
+                    $bar->advance();
+                    // dd($chunk);
+                });
+            });
 
         $this->info("LoadCleanLocationsV2Command took " . (microtime(true) - $start) . " seconds");
         $this->info('Done!');
