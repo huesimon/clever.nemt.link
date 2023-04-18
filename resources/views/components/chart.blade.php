61c0a9a4f3bb2ff1d3decd120647ffc3a25023c6
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2023-04-18 20:55:53 +0200

    bar chart was not usefull at all with this amount of data

--- resources/views/components/chart.blade.php
+++ resources/views/components/chart.blade.php
@@ -2,7 +2,7 @@
     'labels' => [],
     'datasets' => [],
     'title' => 'Chart',
-    // 'chartType' => 'line',
+    'chartType' => 'line',
 ])
 {{-- incase of multiple charts on 1 page, once will prevent the js from being added multiple times --}}
 @once
@@ -15,7 +15,7 @@
 x-data="{
     init() {
         let chart = new Chart(this.$refs.canvas.getContext('2d'), {
-            type: '{{ request()->get('chartType') ?? 'line' }}',
+            type: '{{ $chartType }}',
             data: {
                 labels: {{ Js::from($labels) }},
                 datasets: {{ Js::from($datasets) }},
@@ -48,18 +48,6 @@ class="w-full"
 {{-- headline --}}
 <div class="flex justify-between items-center">
     <h2 class="text-2xl font-bold">{{ $title }}</h2>
-    <div class="flex space-x-2">
-        <a href="{{ request()->fullUrlWithQuery(['chartType' => 'line']) }}" class="text-gray-500 hover:text-gray-700">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10l2-2m0 0l7-7 7 7m-7-7v18M5 21l7-7 7 7"></path>
-            </svg>
-        </a>
-        <a href="{{ request()->fullUrlWithQuery(['chartType' => 'bar']) }}" class="text-gray-500 hover:text-gray-700">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
-            </svg>
-        </a>
-    </div>
 </div>
 <canvas x-ref="canvas" class="rounded-lg bg-white p-8"></canvas>
 </div>
