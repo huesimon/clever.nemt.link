127b4f0b7704ede85b7ab63e15838df5bf744cab
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2023-04-26 21:19:43 +0200

    Parse attributes to button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,9 @@
 @props(['link' => false])
-<button type="button" class="rounded-md border border-gray-200 bg-white px-5 py-2.5">
+<button
+    type="button"
+    class="rounded-md border border-gray-200 bg-white px-5 py-2.5"
+    {{ $attributes }}
+    >
     @if ($link)
         <a href="{{ $link }}">
             {{ $slot }}
