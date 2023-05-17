d68e4ac7cf5a69671ba3b30274b64e2ed65524b3
Simon <simon.d.rasmussen@gmail.com>
2023-05-17 14:01:41 +0200

    dashboard changed name to home

--- routes/web.php
+++ routes/web.php
@@ -157,7 +157,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/home', function () {
         return view('dashboard');
     })->name('dashboard');
 });
