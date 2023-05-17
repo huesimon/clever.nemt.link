097494a28c15e365296b5f4514326d37bf50b1d6
Simon <simon.d.rasmussen@gmail.com>
2023-05-17 18:30:19 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('artisan app:email-users-about-new-locations')->dailyAt('12:00');
 
         $schedule->command('clever:locations')->dailyAt('08:00');
         $schedule->command('clever:locations')->dailyAt('18:00');
