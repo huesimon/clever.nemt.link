ab192485d4efb64664ed457d277e45cf6a041942
Simon <simon.d.rasmussen@gmail.com>
2023-05-25 11:20:27 +0200

    add register button on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,6 +39,10 @@
                     </a>
                 @endif
 
+                <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ml-4"
+                    href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
