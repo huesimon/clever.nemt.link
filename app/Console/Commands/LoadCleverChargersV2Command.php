0e74e309e17b034929b9b85ede9616cb529d7e77
Simon <simon.d.rasmussen@gmail.com>
2023-10-10 16:34:23 +0200

    upgrade to v3 endpoint, refactor to match new structure

--- app/Console/Commands/LoadCleverChargersV2Command.php
+++ app/Console/Commands/LoadCleverChargersV2Command.php
@@ -36,7 +36,7 @@ class LoadCleverChargersV2Command extends Command
         $start = microtime(true);
 
         $this->info('Loading chargers from Clever endpoint...');
-        $url = 'https://clever-app-prod.firebaseio.com/prod/availability/V1.json';
+        $url = 'https://clever-app-prod.firebaseio.com/prod/availability/V3.json';
 
         $response = Http::get($url, [
             'ac' => Company::firstWhere('name', 'Clever')->app_check_token
@@ -54,39 +54,32 @@ class LoadCleverChargersV2Command extends Command
 
         $this->info('Loaded chargers from Clever endpoint');
 
-        $bar = $this->output->createProgressBar(sizeof($response->json()['clever']));
+        $bar = $this->output->createProgressBar(sizeof($response->json()));
 
         $knownChargersWithStatus = Charger::all('evse_id', 'location_external_id', 'status')
-        ->mapWithKeys(function ($item) {
-            return [$item['evse_id'] => $item];
-        })->toArray();
+            ->mapWithKeys(function ($item) {
+                return [$item['evse_id'] => $item];
+            })->toArray();
 
         $insert = [];
-        collect($response->json()['clever'])
-            ->map(function ($item) {
-                return [
-                    'evse_id' => $item['evseId'],
-                    'status' => $item['status'],
-                    'location_external_id' => $item['locationId'],
-                ];
-            })
-            ->chunk(2000)
-            ->each(function (Collection $chunk) use ($knownChargersWithStatus, &$insert, $bar) {
-                $chunk->each(function ($item) use ($knownChargersWithStatus, &$insert, $bar) {
-                    $knownCharger = $knownChargersWithStatus[$item['evse_id']] ?? null;
-
+        collect($response->json())
+            ->each(function ($location) use ($knownChargersWithStatus, &$insert) {
+                foreach ($location['evses'] as $charger) {
+                    $knownCharger = $knownChargersWithStatus[$charger['evseId']] ?? null;
                     if ($knownCharger) {
-                        if ($knownCharger['status'] !== $item['status']) {
-                            $insert[] = $item;
+                        if ($knownCharger['status'] !== $charger['status']) {
+                            $insert[] = [
+                                'evse_id' => $charger['evseId'],
+                                'location_external_id' => $charger['locationId'],
+                                'status' => $charger['status'],
+                            ];
                         }
                     }
-                });
-
-                Charger::upsert($insert, ['evse_id'], ['status', 'updated_at']);
-                $insert = [];
-
-                $bar->advance($chunk->count());
-        });
+                    Charger::upsert($insert, ['evse_id'], ['status', 'updated_at']);
+                    $insert = [];
+                }
+                return $insert;
+            });
 
         $bar->finish();
 
