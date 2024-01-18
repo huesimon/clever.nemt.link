2c51e57aa38a758fa0fd0f5f49265399cdf9a66f
Simon <simon.d.rasmussen@gmail.com>
2024-01-18 16:41:46 +0100

    flex col on smaller screens.

--- resources/views/components/location/card.blade.php
+++ resources/views/components/location/card.blade.php
@@ -98,7 +98,7 @@
                                 <div class="flex flex-col space-y-2 mt-2">
                                     @foreach ($location->chargers as $charger)
                                         {{-- display id and how long the charging_session is --}}
-                                        <div class="flex flex-row items-center gap-4 text-sm">
+                                        <div class="flex flex-col md:flex-row gap-4 text-sm">
                                             @if ($charger->has_star) <span> ⭐ </span> @endif {{ $charger->readable_id }} - <span class="inline-flex items-center rounded-full {{$charger->session_color}} px-2.5 py-0.5 text-xs font-medium text-indigo-800">
                                                 <svg class="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400" fill="currentColor" viewBox="0 0 8 8">
                                                   <circle cx="4" cy="4" r="3" />
