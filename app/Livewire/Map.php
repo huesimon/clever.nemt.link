79e095215428edafe0ef54a4255710bc51e5ba4b
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2024-03-17 19:19:06 +0100

    Move loading placeholder into blade file, so tailwind looks for it during build

--- app/Livewire/Map.php
+++ app/Livewire/Map.php
@@ -18,12 +18,7 @@ class Map extends Component
 
     public function placeholder()
     {
-        return <<<'HTML'
-        <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center">
-            <div class="w-20 h-20 rounded-full bg-blue-500 animate-pulse"></div>
-            <span? class="text-4xl ml-4">Loading...</span>
-        </div>
-        HTML;
+        return view('loading');
     }
 
     public function mount()
---
+++ resources/views/loading.blade.php
@@ -0,0 +1,4 @@
+<div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center">
+    <div class="w-20 h-20 rounded-full bg-blue-500 animate-pulse"></div>
+    <span? class="text-4xl ml-4">Loading...</span>
+</div>
