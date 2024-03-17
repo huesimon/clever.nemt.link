4a99e58b1445080abd2293d620d64e23f243625f
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2024-03-17 08:50:40 +0100

    install reverb

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
